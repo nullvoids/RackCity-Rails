aa30c0cecdd48ca9d22b80d49c16ce733a820cba
sollabeck <kevin.ceballos@gmail.com>
2015-05-26 15:48:30 -0700

    add comment/rack/rating models

---
+++ db/migrate/20150526221211_create_bike_racks.rb
@@ -0,0 +1,9 @@
+class CreateBikeRacks < ActiveRecord::Migration
+  def change
+    create_table :bike_racks do |t|
+      t.integer :rating, :rack_count, :year_installed, default: 0
+      t.string :longitude, :latitude, :address, :location_name
+      t.timestamps null: false
+    end
+  end
+end
